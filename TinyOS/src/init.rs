863f4096ef37fe72ffc25cd4589be8728941ec02
ysj1173886760 <1173886760@qq.com>
2022-02-04 13:58:48 +0800

    initial commit

---
+++ TinyOS/src/init.rs
@@ -0,0 +1,13 @@
+use crate::memory::zero_volatile;
+use crate::link::bss_range;
+use core::ops::Range;
+
+pub unsafe fn runtime_init() {
+    zero_volatile(bss_range());
+    kernel_init();
+}
+
+pub fn kernel_init() {
+    //  ELR_EL2.set(crate::runtime_init::runtime_init as *const () as u64);
+	loop {}
+}

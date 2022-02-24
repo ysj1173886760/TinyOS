d40cff0195d38c53398d7d0b8f95ceb122bd7b5b
ysj1173886760 <1173886760@qq.com>
2022-02-24 19:18:19 +0800

    implement kalloc

--- TinyOS/src/mm/kalloc.rs
+++ TinyOS/src/mm/kalloc.rs
@@ -1,95 +1,84 @@
-// use core::ptr::{self, NonNull};
+use core::ptr::{self, NonNull};
 
-// use crate::spinlock::SpinLock;
+use crate::spinlock::SpinLock;
 
-// // indicate that the page is aligned to 4096
-// pub trait PageAligned {}
+// indicate that the page is aligned to 4096
+pub trait PageAligned {}
 
-// extern "C" {
-//     // first address after kernel.
-//     // defined by kernel.ld.
-//     fn end();
-// }
+extern "C" {
+    // first address after kernel.
+    // defined by kernel.ld.
+    fn end();
+}
 
-// #[repr(C)]
-// pub struct Frame {
-//     next: Option<NonNull<Frame>>,
-// }
+#[repr(C)]
+pub struct Frame {
+    next: Option<NonNull<Frame>>,
+}
 
-// // indicate that our frame is always page-aligned
-// impl PageAligned for Frame{}
+// indicate that our frame is always page-aligned
+impl PageAligned for Frame{}
 
-// impl Frame {
-//     // convert raw addr to frame
-//     unsafe fn new(pa: usize) -> NonNull<Frame> {
-//         let frame_ptr = pa as *mut Frame;
-//         // construct the frame at frame_ptr
-//         ptr::write(frame_ptr, Frame{ next: None });
-//         // construct the NonullPtr pointing to the frame we previously created
-//         NonNull::new(frame_ptr).unwrap()
-//     }
+impl Frame {
+    // convert raw addr to frame
+    unsafe fn new(pa: usize) -> NonNull<Frame> {
+        let frame_ptr = pa as *mut Frame;
+        // construct the frame at frame_ptr
+        ptr::write(frame_ptr, Frame{ next: None });
+        // construct the NonullPtr pointing to the frame we previously created
+        NonNull::new(frame_ptr).unwrap()
+    }
 
-//     fn set(&mut self, value: Option<NonNull<Frame>>) {
-//         self.next = value;
-//     }
+    fn set(&mut self, value: Option<NonNull<Frame>>) {
+        self.next = value;
+    }
 
-//     fn take_next(&mut self) -> Option<NonNull<Frame>> {
-//         self.next
-//     }
-// }
+    fn take_next(&mut self) -> Option<NonNull<Frame>> {
+        self.next
+    }
+}
 
-// unsafe impl Send for Frame {}
+unsafe impl Send for Frame {}
 
-// unsafe impl Sync for Frame {}
+type FrameList = Frame;
 
-// type FrameList = Frame;
+static KMEM: SpinLock<FrameList> = SpinLock::new(FrameList { next: None }, "kmem");
 
-// struct Kmem {
-//     lock: SpinLock,
-//     frameList: FrameList,
-// }
+unsafe fn free_range(start_addr: usize, end_addr: usize) {
+    let start = super::pg_round_up(start_addr);
+    for pa in (start_addr..end_addr).step_by(super::PGSIZE) {
+        kfree(Frame::new(pa));
+    }
+}
 
-// static KMEM: Kmem = Kmem {
-//     lock: SpinLock::new("kmem"),
-//     frameList: FrameList { next: None }
-// };
+pub fn kinit() {
+    crate::println!("kinit: end={:#x}", end as usize);
+    unsafe {
+        free_range(end as usize, crate::consts::memlayout::PHYSTOP)
+    }
+}
 
-// unsafe fn free_range(start_addr: usize, end_addr: usize) {
-//     let start = super::pg_round_up(start_addr);
-//     for pa in (start_addr..end_addr).step_by(super::PGSIZE) {
-//         kfree(Frame::new(pa));
-//     }
-// }
+pub fn kfree(frame: NonNull<Frame>) {
+    let mut head = KMEM.lock();
+    // we need to convert frame to mutable
+    unsafe {
+        let mut frame = NonNull::new(frame.as_ptr()).unwrap();
+        frame.as_mut().set(head.take_next());
+        head.set(Some(frame));
+    }
+}
 
-// pub fn kinit() {
-//     crate::println!("kinit: end={:#x}", end as usize);
-//     unsafe {
-//         free_range(end as usize, crate::consts::memlayout::PHYSTOP)
-//     }
-// }
+pub fn kalloc() -> Option<NonNull<Frame>> {
+    let mut head = KMEM.lock();
+    let frame = head.take_next();
+    if let Some(mut frame_ptr) = frame {
+        unsafe {
+            head.set(frame_ptr.as_mut().take_next())
+        }
+    }
 
-// pub fn kfree(frame: NonNull<Frame>) {
-//     KMEM.lock.acquire();
-//     unsafe {
-//         frame.as_mut().set(KMEM.frameList.take_next());
-//     }
-//     KMEM.frameList.set(Some(frame));
-//     KMEM.lock.release();
-// }
-
-// pub fn kalloc() -> Option<NonNull<Frame>> {
-    // KMEM.lock.acquire();
-    // let frame = KMEM.frameList.take_next();
-    // if let Some(mut frame_ptr) = frame {
-    //     unsafe {
-    //         KMEM.frameList.set(frame_ptr.as_mut().take_next())
-    //     }
-    // }
-
-    // KMEM.lock.release();
-
-    // match frame {
-    //     Some(frame_ptr) => Some(frame_ptr),
-    //     None => None,
-    // }
-// }
+    match frame {
+        Some(frame_ptr) => Some(frame_ptr),
+        None => None,
+    }
+}
